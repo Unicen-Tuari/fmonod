01ad0ac1d5cd85a6e275b2d870b6dd21490e8b0f
Facundo Monod Nuñez <facundo.monod94@gmail.com>
2017-05-01 02:36:46 -0300

    Subo implementacion de 2 jugadores Closes #23
    Borre un par de lineas del modal que hacian el autofocus y agregue
    comentarios para delimitarlo

--- Juego Javascript/js/main.js
+++ Juego Javascript/js/main.js
@@ -64,20 +64,29 @@ function juego(id1,jugada1,id2,jugada2){
 function juego1vsIA(){
   let jugador1 = document.querySelector('input[name = "optradio"]:checked').value;
   let jugadorIA = jugadaIA();
-  // alert("You picked " + jugador1);
-  // alert(juego("1",jugador1,"IA",jugadaIA()));
-  $('#Modal1vsIA').on('shown.bs.modal', function () {
-    $('#myInput').focus()
-  })
-  let resultadoJuego = juego("1",jugador1,"IA",jugadorIA);
+  let resultadoJuego1vsIA = juego("1",jugador1,"IA",jugadorIA);
   let modalBody1vsIA = document.getElementById('modal-body-1vsIA');
-  modalBody1vsIA.innerHTML = resultadoJuego + "<br>";
+  modalBody1vsIA.innerHTML = resultadoJuego1vsIA + "<br>";
   modalBody1vsIA.innerHTML += "Jugador 1 escogio: " + jugador1 + "<br>";
   modalBody1vsIA.innerHTML += "Jugador IA escogio: " + jugadorIA;
 }
 
 function juego1vs2(){
+  let jugador1 = jugadaJ1();
+  let jugador2 = document.querySelector('input[name = "optradioy"]:checked').value;
+  let resultadoJuego1vs2 = juego("1",jugador1,"2",jugador2);
+  let modalBody1vs2 = document.getElementById('modal-body-1vs2');
+  modalBody1vs2.innerHTML = resultadoJuego1vs2 + "<br>";
+  modalBody1vs2.innerHTML += "Jugador 1 escogio: " + jugador1 + "<br>";
+  modalBody1vs2.innerHTML += "Jugador 2 escogio: " + jugador2;
+}
 
+function jugadaJ1(){
+let jugador1 = document.querySelector('input[name = "optradiox"]:checked').value;
+let menuJ1 = document.getElementById('j1');
+let menuJ2 = document.getElementById('j2');
+switchDivDisplay(menuJ1,menuJ2);
+return jugador1;
 }
 
 function jugadaIA(){
@@ -102,11 +111,23 @@ function jugadaIA(){
       return eleccionIA;
 }
 
+function switchDivDisplay(div1,div2){
+  div1.style.display = "none";
+  div2.style.display = "block";
+}
+
 function main(){
   let contadorPartidasJ1 = 0;
   let contadorPartidasIA = 0;
   let btnjugarvsia = document.getElementById('jugar-vs-ia');
   btnjugarvsia.addEventListener("click", juego1vsIA);
-
+  let btnjugarj1 = document.getElementById('jugar-j1');
+  btnjugarj1.addEventListener("click", jugadaJ1);
+  let btnjugarj2 = document.getElementById('jugar-j2');
+  btnjugarj2.addEventListener("click", juego1vs2);
+  let btnclosemodal = document.getElementById('close1vs2')
+  btnclosemodal.addEventListener("click",function(e) {
+    switchDivDisplay(document.getElementById('j2'),document.getElementById('j1'));
+  } );
 }
 main();
